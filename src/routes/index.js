388db49def82529fd4d14d1a4220396048a537f7
sp@decodeup.email <sp@decodeup.email>
2025-05-22 14:46:14 +0530

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,9 @@ const userRoute = require("./auth.routes");
 const jobRoute = require("./job.routes");
 
 router.use("/user", userRoute); // User Routes
-router.use("/job", jobRoute);   // Job Routes
+router.use("/job", jobRoute);   // Job Routes 
 
-// Root Route
+// Root Route 
 router.get("/", async (req, res) => {
   return res.json({ message: "Welcome to the JOB Backend APIs!" });
 });
